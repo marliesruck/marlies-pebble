86fc9c32a5601cce310ff86a2b7bb8328a53f56a
Marlies Ruck <mruck@andrew.cmu.edu>
2014-03-24 15:10:26 -0400

    Added locks about malloc lib
    Running tests to see if this solves malloc failing issue.

--- kern/lib/malloc_wrappers.c
+++ kern/lib/malloc_wrappers.c
@@ -8,43 +8,65 @@ static mutex_s allocator_lock = MUTEX_INITIALIZER(allocator_lock);
 /* safe versions of malloc functions */
 void *malloc(size_t size)
 {
-  return _malloc(size); 
+  mutex_lock(&allocator_lock);
+  void *addr = _malloc(size); 
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void *memalign(size_t alignment, size_t size)
 {
-  return _memalign(alignment,size);
+  mutex_lock(&allocator_lock);
+  void *addr = _memalign(alignment,size);
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void *calloc(size_t nelt, size_t eltsize)
 {
-  return _calloc(nelt,eltsize);
+  mutex_lock(&allocator_lock);
+  void *addr = _calloc(nelt,eltsize);
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void *realloc(void *buf, size_t new_size)
 {
-  return _realloc(buf,new_size);
+  mutex_lock(&allocator_lock);
+  void *addr = _realloc(buf,new_size);
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void free(void *buf)
 {
+  mutex_lock(&allocator_lock);
   _free(buf);
+  mutex_unlock(&allocator_lock);
   return;
 }
 
 void *smalloc(size_t size)
 {
-  return _smalloc(size);
+  mutex_lock(&allocator_lock);
+  void *addr = _smalloc(size);
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void *smemalign(size_t alignment, size_t size)
 {
-  return _smemalign(alignment,size);
+  mutex_lock(&allocator_lock);
+  void *addr = _smemalign(alignment,size);
+  mutex_unlock(&allocator_lock);
+  return addr;
 }
 
 void sfree(void *buf, size_t size)
 {
+  mutex_lock(&allocator_lock);
   _sfree(buf,size);
+  mutex_unlock(&allocator_lock);
   return;
 }
 
