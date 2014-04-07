b6c5671d1f0d8ccd2748c2aa4386b576f460dba0
Marlies Ruck <mruck@andrew.cmu.edu>
2014-04-06 22:00:07 -0400

    Modifed fault wrappers

--- kern/entry/faults/fault_wrappers.h
+++ kern/entry/faults/fault_wrappers.h
@@ -11,26 +11,26 @@
 
 #include <ureg.h>
 
-int asm_int_divzero(*ureg_t);
-int asm_int_debug(*ureg_t);
+int asm_int_divzero(ureg_t *ureg);
+int asm_int_debug(ureg_t *ureg);
 void asm_int_nmi(void);
-int asm_int_breakpoint(*ureg_t);
-int asm_int_overflow(*ureg_t);
-int asm_int_bound(*ureg_t);
-int asm_int_undef_opcode(*ureg_t);
-int asm_int_device_unavail(*ureg_t);
+int asm_int_breakpoint(ureg_t *ureg);
+int asm_int_overflow(ureg_t *ureg);
+int asm_int_bound(ureg_t *ureg);
+int asm_int_undef_opcode(ureg_t *ureg);
+int asm_int_device_unavail(ureg_t *ureg);
 void asm_int_double_fault(void);
 void asm_int_cso(void);
 void asm_int_tss(void);
-int asm_int_seg_not_present(*ureg_t);
-int asm_int_stack_seg(*ureg_t);
-int asm_int_gen_prot(*ureg_t);
-int asm_int_float(*ureg_t);
-int asm_int_align(*ureg_t);
+int asm_int_seg_not_present(ureg_t *ureg);
+int asm_int_stack_seg(ureg_t *ureg);
+int asm_int_gen_prot(ureg_t *ureg);
+int asm_int_float(ureg_t *ureg);
+int asm_int_align(ureg_t *ureg);
 void asm_int_machine_check(void);
-int asm_int_simd(*ureg_t);
+int asm_int_simd(ureg_t *ureg);
 void asm_int_generic(void);
-int asm_int_page_fault(*ureg_t);
+int asm_int_page_fault(ureg_t *ureg);
 
 #endif /* __FAULT_WRAPPERS_H__ */
 
