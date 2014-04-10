edd5f13e48a8afed8dd39147ee255a90b8c73837
Marlies Ruck <mruck@andrew.cmu.edu>
2014-04-10 17:39:01 -0400

    Added cr_util to config.mk and fixed comments

--- kern/inc/cr_util.h
+++ kern/inc/cr_util.h
@@ -1,4 +1,4 @@
-/** @file page_util.h
+/** @file cr_util.h
  *
  *  @brief Declares x86 specific prototypes for paging and protections.
  *
@@ -6,12 +6,12 @@
  *  @author Marlies Ruck (mruck)
  **/
 
-#ifndef __PAGE_UTIL_H__
-#define __PAGE_UTIL_H__
+#ifndef __CR_UTIL_H__
+#define __CR_UTIL_H__
 
 int interrupts_enabled(void);
 void enable_write_protect(void);
 void enable_paging(void);
 void disable_paging(void);
 
-#endif /* __PAGE_UTIL_H__ */
+#endif /* __CR_UTIL_H__ */
